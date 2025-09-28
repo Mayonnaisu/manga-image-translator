745de4bed14db60e05b10b4272f718545d8a5871
Mayonnaisu <116666368+Mayonnaisu@users.noreply.github.com>
2025-09-28 19:06:03 +0700

    Fix error when installing winget

--- MIT-installer.ps1
+++ MIT-installer.ps1
@@ -1,7 +1,28 @@
 # Install Microsoft C++ Build Tools
 Write-Host "Installing Microsoft C++ Build Tools..." -ForegroundColor Yellow
 
-winget install Microsoft.VisualStudio.2022.BuildTools --force --override "--wait --passive --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows11SDK.26100" # Replace 'Windows11SDK.26100' with 'Windows10SDK' for Windows 10
+New-Item -Path "C:\Temp" -ItemType Directory -Force
+
+$sourceUrl = "https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+$destinationPath = "C:\Temp\WinGet.msixbundle"
+
+if (Test-Path $destinationPath) {
+    Write-Host "File '$destinationPath' already exists. Skipping download."
+} else {
+    Write-Host "File not found. Initiating download..."
+    try {
+        Invoke-WebRequest -Uri $sourceUrl -OutFile $destinationPath -ErrorAction Stop
+        Write-Host "Download completed successfully to '$destinationPath'."
+    } catch {
+        Write-Host "Error during download: $($_.Exception.Message)"
+    }
+}
+
+Add-AppxPackage "C:\Temp\WinGet.msixbundle"
+
+winget source update
+
+winget install Microsoft.VisualStudio.2022.BuildTools --force --override "--wait --passive --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows11SDK.26100" --accept-source-agreements --accept-package-agreements # Replace 'Windows11SDK.26100' with 'Windows10SDK' for Windows 10
 
 Write-Host "`nMicrosoft C++ Build Tools Installed." -ForegroundColor DarkGreen
 
@@ -14,9 +35,9 @@ Write-Host "`nPyenv Windows Installed." -ForegroundColor DarkGreen
 # Since it's required to reopen PowerShell after installing Pyenv Windows, I'll just launch PowerShell in a new window to install Python 3.10.11 with Pyenv, set up Python virtual environment, & install MIT dependencies.
 Write-Host "`nInstalling Python, Setting Up Python Virtual Environment, & Installing MIT Dependencies..." -ForegroundColor Yellow
 
-$ScriptPath = "$env:Temp\commands.ps1"
+$ScriptPath2 = "$env:Temp\commands2.ps1"
 
-$Commands = @"
+$Commands2 = @"
 Write-Host '$PWD'
 Write-Host "`nInstalling Python 3.10.11." -ForegroundColor DarkGreen
 pyenv --version
@@ -32,11 +53,11 @@ pip install -r requirements.txt
 Write-Host "`nMIT Dependencies Installed." -ForegroundColor DarkGreen
 "@
 
-Set-Content -Path $ScriptPath -Value $Commands
+Set-Content -Path $ScriptPath2 -Value $Commands2
 
-Start-Process powershell -ArgumentList '-Command', '& "$env:Temp\commands.ps1"; Start-Sleep -Seconds 3' -Wait
+Start-Process powershell -ArgumentList '-Command', '& "$env:Temp\commands2.ps1"; Start-Sleep -Seconds 3' -Wait
 
-Remove-Item -Path $ScriptPath -Force
+Remove-Item -Path $ScriptPath2 -Force
 
 Write-Host "`nPython Installed, Virtual Environment Created, & MIT Dependencies Installed." -ForegroundColor DarkGreen
 
