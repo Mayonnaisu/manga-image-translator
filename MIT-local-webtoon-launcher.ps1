688735a969922905b38f07fe9b790ba9be2d7b8f
Mayonnaisu <116666368+Mayonnaisu@users.noreply.github.com>
2025-09-30 20:31:52 +0700

    Add -UseBasicParsing

--- MIT-local-webtoon-launcher.ps1
+++ MIT-local-webtoon-launcher.ps1
@@ -18,7 +18,7 @@ if (Test-Path $ToolsPath) {
     Write-Host "`nFile not found. Initiating download..."
     New-Item -Path ".\my_tools" -ItemType Directory -Force
     try {
-        Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Mayonnaisu/manga-image-translator/refs/heads/main/my_tools/image-merger_all.py" -OutFile $ToolsPath -ErrorAction Stop
+        Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/Mayonnaisu/manga-image-translator/refs/heads/main/my_tools/image-merger_all.py" -OutFile $ToolsPath -ErrorAction Stop
         Write-Host "`nDownload completed successfully to '$([System.IO.Path]::GetFullPath($ToolsPath))'."
     } catch {
         Write-Host "`nError during download: $($_.Exception.Message)"
