749757217fb1afc3a48c16ef09707c4326b6b8b3
Mayonnaisu <116666368+Mayonnaisu@users.noreply.github.com>
2025-10-02 00:11:18 +0700

    Add image splitting function

--- MIT-local-webtoon-launcher.ps1
+++ MIT-local-webtoon-launcher.ps1
@@ -8,43 +8,36 @@ Write-Host "Activating Virtual Environment..." -ForegroundColor Yellow
 Write-Host "`nVirtual Environment Activated." -ForegroundColor Green
 
 # Merge all images in each chapter folder into one respectively
-Write-Host "`nMerging All Images in Each Subfolder..." -ForegroundColor Yellow
+Write-Host "`nMerging All Input Images in Each Subfolder..." -ForegroundColor Yellow
 
-$ToolsPath = ".\my_tools\image-merger_all.py"
+python .\my_tools\image-merger_all.py $InputPath
 
-if (Test-Path $ToolsPath) {
-    python .\my_tools\image-merger_all.py $InputPath
-} else {
-    Write-Host "`nFile not found. Initiating download..."
-    New-Item -Path ".\my_tools" -ItemType Directory -Force
-    try {
-        Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/Mayonnaisu/manga-image-translator/refs/heads/main/my_tools/image-merger_all.py" -OutFile $ToolsPath -ErrorAction Stop
-        Write-Host "`nDownload completed successfully to '$([System.IO.Path]::GetFullPath($ToolsPath))'."
-        python .\my_tools\image-merger_all.py $InputPath
-    } catch {
-        Write-Host "`nError during download: $($_.Exception.Message)"
-    }
-}
-
-Write-Host "`nAll Images Merged and Saved to $($InputPath)_combined." -ForegroundColor Green
+Write-Host "`nAll Input Images Merged and Saved to $($InputPath)_combined." -ForegroundColor Green
 
 # Run Manga Image Translator in local (batch) mode
 Write-Host "`nRunning Manga Image Translator in Local Mode... " -ForegroundColor Yellow
 
 python -m manga_translator local -v -i "$($InputPath)_combined" --config-file ".\examples\my-config.json"
 
+# Split all images in each chapter folder into the number of input/original images.
+Write-Host "`nSplitting All Translated Images in Each Subfolder..." -ForegroundColor Yellow
+
+python .\my_tools\image-splitter.py "$($InputPath)_combined-translated" 
+
+Write-Host "`nAll Translated Images Splitted and Saved to $($InputPath)-translated." -ForegroundColor Green
+
 # Show delete confirmation for merged images
 while ($true) {
-    Write-Host "`nDo you want to delete merged images in '$($InputPath)_combined'?" -ForegroundColor Black -BackgroundColor Yellow
+    Write-Host "`nDo you want to delete merged input images in '$($InputPath)_combined'?" -ForegroundColor Black -BackgroundColor Yellow
     $confirmRemoval = Read-Host -Prompt "ENTER y or n"
 
     if ($confirmRemoval -ieq 'y') {
         Write-Host "`nDeleting Merged Images... " -ForegroundColor Yellow
         Remove-Item -Path "$($InputPath)_combined" -Recurse -Force -Confirm:$false
-        Write-Host "`nMerged Images Deleted." -ForegroundColor Green
+        Write-Host "`nMerged Input Images Deleted." -ForegroundColor Green
         break
     } ElseIf ($confirmRemoval -ieq 'n') {
-        Write-Host "`nMerged Images Retained." -ForegroundColor Yellow
+        Write-Host "`nMerged Input Images Retained." -ForegroundColor Yellow
         break
     } else {
         Write-Host "`nInvalid input. Please type 'y' or 'n', then press ENTER." -ForegroundColor Red
