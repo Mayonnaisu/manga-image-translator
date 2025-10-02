dec571110769d37f7e200e5cc4053f815f4401ac
Mayonnaisu <116666368+Mayonnaisu@users.noreply.github.com>
2025-10-03 06:27:44 +0700

    Improve error handling

--- MIT-local-launcher.ps1
+++ MIT-local-launcher.ps1
@@ -1,17 +1,43 @@
-﻿# Read input path from MIT-input-path.txt
-$InputPath = Get-Content -Path ".\MIT-input-path.txt" | ForEach-Object { $ExecutionContext.InvokeCommand.ExpandString($_) }
+﻿# Change global preference for all error to terminate the process
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
+
+# Read input path from MIT-input-path.txt
+try {
+    $InputPath = Get-Content -Path ".\MIT-input-path.txt" | ForEach-Object { $ExecutionContext.InvokeCommand.ExpandString($_) }
+    
+    if (-not (Test-Path -Path $InputPath)) {
+        Throw "$InputPath does not exist!"
+    }
+} catch {
+    Write-Host "`nERROR: $($_.Exception.Message)" -ForegroundColor Red
+}
 
 # Activate Python venv with another PowerShell script
-Write-Host "Activating Virtual Environment..." -ForegroundColor Yellow
+try {
+    Write-Host "`nActivating Virtual Environment..." -ForegroundColor Yellow
 
-.\venv\Scripts\Activate.ps1
+    .\venv\Scripts\Activate.ps1
 
-Write-Host "`nVirtual Environment Activated" -ForegroundColor Green
+    Write-Host "`nVirtual Environment Activated." -ForegroundColor Green
+} catch {
+    Write-Host "`nERROR: Failed to Activate Virtual Environment!`n$($_.Exception.Message)" -ForegroundColor Red
+}
 
 # Run Manga Image Translator in local (batch) mode
-Write-Host "`nRunning Manga Image Translator in Local Mode... " -ForegroundColor Yellow
+try {
+    Write-Host "`nRunning Manga Image Translator in Local Mode... " -ForegroundColor Yellow
 
-python -m manga_translator local -v -i $InputPath --config-file ".\examples\my-config.json"
+    python -m manga_translator local -v -i $InputPath --config-file ".\examples\my-config.json"
+
+    if ($LASTEXITCODE -ne 0) {
+        Throw "Manga Image Translator Run into ERROR!`nEXIT CODE: $LASTEXITCODE."
+    } else {
+        Write-Host "`nAll Images Translated & Saved to $($InputPath)_combined-translated" -ForegroundColor Green
+    }
+} catch {
+    Write-Host "`nERROR: $($_.Exception.Message)" -ForegroundColor Red
+}
 
 # Show exit confirmation
 Write-Host "`nPress Enter to exit" -ForegroundColor Cyan -NoNewLine
