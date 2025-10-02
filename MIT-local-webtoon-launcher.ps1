65e7c8f5af55dbe9f15a68414d7238203b2121f6
Mayonnaisu <116666368+Mayonnaisu@users.noreply.github.com>
2025-10-02 20:28:01 +0700

    Improve error handling

--- MIT-local-webtoon-launcher.ps1
+++ MIT-local-webtoon-launcher.ps1
@@ -1,31 +1,56 @@
+# Change global preference for all error to terminate the process
+$ErrorActionPreference = "Stop"
+
 # Read input path from MIT-input-path.txt
-$InputPath = Get-Content -Path ".\MIT-input-path.txt" | ForEach-Object { $ExecutionContext.InvokeCommand.ExpandString($_) }
+try {
+    $InputPath = Get-Content -Path ".\MIT-input-path.txt" | ForEach-Object { $ExecutionContext.InvokeCommand.ExpandString($_) }
+} catch {
+    Write-Warning "`nFailed to Read Input Path from MIT-input-path.txt! Error: $($_.Exception.Message)" -ForegroundColor Red
+}
 
 # Activate Python venv with another PowerShell script
-Write-Host "Activating Virtual Environment..." -ForegroundColor Yellow
+try {
+    Write-Host "Activating Virtual Environment..." -ForegroundColor Yellow
 
-.\venv\Scripts\Activate.ps1
+    .\venv\Scripts\Activate.ps1
 
-Write-Host "`nVirtual Environment Activated." -ForegroundColor Green
+    Write-Host "`nVirtual Environment Activated." -ForegroundColor Green
+} catch {
+    Write-Warning "`nFailed to Activate Virtual Environment! Error: $($_.Exception.Message)" -ForegroundColor Red
+}
 
 # Merge all images in each chapter folder into one respectively
-Write-Host "`nMerging All Input Images in Each Subfolder..." -ForegroundColor Yellow
+try {
+    Write-Host "`nMerging All Input Images in Each Subfolder..." -ForegroundColor Yellow
 
-python .\my_tools\image-merger_all.py $InputPath
+    python .\my_tools\image-merger_all.py $InputPath
 
-Write-Host "`nAll Input Images Merged and Saved to $($InputPath)_combined." -ForegroundColor Green
+    Write-Host "`nAll Input Images Merged and Saved to $($InputPath)_combined." -ForegroundColor Green
+} catch {
+    Write-Warning "`nFailed to Activate Virtual Environment! Error: $($_.Exception.Message)" -ForegroundColor Red
+}
 
 # Run Manga Image Translator in local (batch) mode
-Write-Host "`nRunning Manga Image Translator in Local Mode... " -ForegroundColor Yellow
+try {
+    Write-Host "`nRunning Manga Image Translator in Local Mode... " -ForegroundColor Yellow
 
-python -m manga_translator local -v -i "$($InputPath)_combined" --config-file ".\examples\my-config.json"
+    python -m manga_translator local -v -i "$($InputPath)_combined" --config-file ".\examples\my-config.json"
+
+    Write-Host "`nAll Images Translated & Saved to $($InputPath)_combined-translated" -ForegroundColor Green
+} catch {
+    Write-Warning "`nManga Image Translator Run into ERROR! Error: $($_.Exception.Message)" -ForegroundColor Red
+}
 
 # Split all images in each chapter folder into the number of input/original images.
-Write-Host "`nSplitting All Translated Images in Each Subfolder..." -ForegroundColor Yellow
+try {
+    Write-Host "`nSplitting All Translated Images in Each Subfolder..." -ForegroundColor Yellow
 
-python .\my_tools\image-splitter.py "$($InputPath)_combined-translated" 
+    python .\my_tools\image-splitter.py "$($InputPath)_combined-translated" 
 
-Write-Host "`nAll Translated Images Splitted and Saved to $($InputPath)-translated." -ForegroundColor Green
+    Write-Host "`nAll Translated Images Splitted and Saved to $($InputPath)-translated." -ForegroundColor Green
+} catch {
+    Write-Warning "`nERROR during Image Splitting! Error: $($_.Exception.Message)" -ForegroundColor Red
+}
 
 # Show delete confirmation for merged images
 while ($true) {
