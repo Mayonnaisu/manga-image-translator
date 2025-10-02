79595761c65c7d1593bb67ce5098b8330abdeb0b
Mayonnaisu <116666368+Mayonnaisu@users.noreply.github.com>
2025-10-03 06:31:48 +0700

    Improve error handling

--- MIT-web-launcher.ps1
+++ MIT-web-launcher.ps1
@@ -1,11 +1,27 @@
+# Change global preference for all error to terminate the process
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
+
 # Activate Python venv with another PowerShell script
-Write-Host "Activating Virtual Environment..." -ForegroundColor Yellow
+try {
+    Write-Host "`nActivating Virtual Environment..." -ForegroundColor Yellow
 
-.\venv\Scripts\Activate.ps1
+    .\venv\Scripts\Activate.ps1
 
-Write-Host "`nVirtual Environment Activated." -ForegroundColor Green
+    Write-Host "`nVirtual Environment Activated." -ForegroundColor Green
+} catch {
+    Write-Host "`nERROR: Failed to Activate Virtual Environment!`n$($_.Exception.Message)" -ForegroundColor Red
+}
 
 # Run Manga Image Translator in web mode
-Write-Host "`nRunning Manga Image Translator in Web Mode... " -ForegroundColor Yellow
+try {
+    Write-Host "`nRunning Manga Image Translator in Web Mode... " -ForegroundColor Yellow
 
-python ./server/main.py
+    python .\server\main.py
+
+    if ($LASTEXITCODE -ne 0) {
+        Throw "Manga Image Translator Run into ERROR!`nEXIT CODE: $LASTEXITCODE."
+    }
+} catch {
+    Write-Host "`nERROR: $($_.Exception.Message)" -ForegroundColor Red
+}
