e3a0ae16e0a5cc2059fa4ce72b90e0057d9e81bf
Mayonnaisu <116666368+Mayonnaisu@users.noreply.github.com>
2025-10-04 00:08:34 +0700

    Adjust to new tools' names & argument

--- MIT-local-webtoon-launcher.ps1
+++ MIT-local-webtoon-launcher.ps1
@@ -28,12 +28,12 @@ try {
 try {
     Write-Host "`nMerging All Input Images in Each Subfolder..." -ForegroundColor Yellow
 
-    python .\my_tools\image-merger_all.py $InputPath
+    python .\my_tools\image_merger.py $InputPath 2
 
     if ($LASTEXITCODE -ne 0) {
         Throw "Failed to Merge Images!`nEXIT CODE: $LASTEXITCODE."
     } else {
-        Write-Host "`nAll Input Images Merged and Saved to $($InputPath)_combined." -ForegroundColor Green
+        Write-Host "`nAll Input Images Merged and Saved to $($InputPath)_merged." -ForegroundColor Green
     }
 } catch {
     Write-Host "`nERROR: $($_.Exception.Message)" -ForegroundColor Red
@@ -43,12 +43,12 @@ try {
 try {
     Write-Host "`nRunning Manga Image Translator in Local Mode... " -ForegroundColor Yellow
 
-    python -m manga_translator local -v -i "$($InputPath)_combined" --config-file ".\examples\my-config.json"
+    python -m manga_translator local -v -i "$($InputPath)_merged" --config-file ".\examples\my-config.json"
 
     if ($LASTEXITCODE -ne 0) {
         Throw "Manga Image Translator Run into ERROR!`nEXIT CODE: $LASTEXITCODE."
     } else {
-        Write-Host "`nAll Images Translated & Saved to $($InputPath)_combined-translated" -ForegroundColor Green
+        Write-Host "`nAll Images Translated & Saved to $($InputPath)_merged-translated" -ForegroundColor Green
     }
 } catch {
     Write-Host "`nERROR: $($_.Exception.Message)" -ForegroundColor Red
@@ -58,7 +58,7 @@ try {
 try {
     Write-Host "`nSplitting All Translated Images in Each Subfolder..." -ForegroundColor Yellow
 
-    python .\my_tools\image-splitter.py "$($InputPath)_combined-translated"
+    python .\my_tools\image_splitter.py "$($InputPath)_merged-translated"
 
     if ($LASTEXITCODE -ne 0) {
         Throw "Failed to Split Images!`nEXIT CODE: $LASTEXITCODE."
@@ -71,12 +71,12 @@ try {
 
 # Show delete confirmation for merged images
 while ($true) {
-    Write-Host "`nDo you want to delete merged input images in '$($InputPath)_combined'?" -ForegroundColor Black -BackgroundColor Yellow
+    Write-Host "`nDo you want to delete merged input images in '$($InputPath)_merged'?" -ForegroundColor Black -BackgroundColor Yellow
     $confirmRemoval = Read-Host -Prompt "ENTER y or n"
 
     if ($confirmRemoval -ieq 'y') {
         Write-Host "`nDeleting Merged Images... " -ForegroundColor Yellow
-        Remove-Item -Path "$($InputPath)_combined" -Recurse -Force -Confirm:$false
+        Remove-Item -Path "$($InputPath)_merged" -Recurse -Force -Confirm:$false
         Write-Host "`nMerged Input Images Deleted." -ForegroundColor Green
         break
     } ElseIf ($confirmRemoval -ieq 'n') {
