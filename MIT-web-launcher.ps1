c086a1c47de7c5171872f80d2030a85913cb88ca
Mayonnaisu <116666368+Mayonnaisu@users.noreply.github.com>
2025-10-22 22:58:58 +0700

    Fix web launcer failure to clean up result folder v1

--- MIT-web-launcher.ps1
+++ MIT-web-launcher.ps1
@@ -9,6 +9,66 @@ $CleanMITresultFolder = $True
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $True
 
+# Create temporary error log file
+$ErrorLogPath = ".\Temp\log_errors-launcher-web.txt"
+New-Item -Path ".\Temp" -ItemType Directory -Force | Out-Null
+
+# Define another script to run MIT server
+$MITserverPath = ".\my_tools\MIT-server.ps1"
+
+$MITserver = @'
+param(
+    [string]$ServerHost,
+    [string]$Port
+)
+
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $True
+$ErrorLogPath = ".\Temp\log_errors-launcher-web.txt"
+
+# Activate Python venv with another PowerShell script
+try {
+    Write-Host "`nActivating Virtual Environment..." -ForegroundColor Yellow
+    
+    .\venv\Scripts\Activate.ps1
+
+    Write-Host "`nVirtual Environment Activated." -ForegroundColor Green
+} catch {
+    Throw "ERROR: Failed to Activate Virtual Environment!`n$($_.Exception.Message)" | Out-File -FilePath $ErrorLogPath
+    exit 1
+}
+
+# Run Manga Image Translator in web mode
+try {
+    Write-Host "`nRunning Manga Image Translator in Web Mode... " -ForegroundColor Yellow
+
+    if ($ServerHost -eq "IP Address") {
+        $Bind = Get-NetIPAddress | Where-Object {$_.AddressFamily -eq 'IPv4' -and $_.IPAddress -like '192.168.1.*'} | Select-Object -ExpandProperty IPAddress
+    } else {
+        $Bind = $ServerHost
+    }
+
+    python .\server\main.py --host $Bind --port $Port
+
+    if ($LASTEXITCODE -ne 0) {
+        Throw "Manga Image Translator Ran into Exception!`n$($_.Exception.Message)`nEXIT CODE: $LASTEXITCODE."
+    }
+} catch {
+    if ('$ServerHost' -eq "IP Address") {
+        # handle the dynamic IP change when the server is running
+        $NewBind = Get-NetIPAddress | Where-Object {$_.AddressFamily -eq 'IPv4' -and $_.IPAddress -like '192.168.1.*'} | Select-Object -ExpandProperty IPAddress
+        if ($Bind -ne $NewBind) {
+            python .\server\main.py --host $NewBind  --port $Port
+        }
+    } else {
+        Throw "ERROR: $($_.Exception.Message)" | Out-File -FilePath $ErrorLogPath
+        exit 1
+    }
+}
+
+exit 0
+'@
+
 # Start the launcher
 try {
     # Get & display PowerShell version
@@ -18,49 +78,37 @@ try {
     # Show launching message
     Write-Host "`nLaunching..." -ForegroundColor Yellow
     
-    # Activate Python venv with another PowerShell script
-    try {
-        Write-Host "`nActivating Virtual Environment..." -ForegroundColor Yellow
+    # Create & run MIT-server.ps1
+    Set-Content -Path $MITserverPath -Value $MITserver
 
-        .\venv\Scripts\Activate.ps1
+    $process = Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile -File `"$MITserverPath`" -ServerHost `"$ServerHost`" -Port $Port" -PassThru
 
-        Write-Host "`nVirtual Environment Activated." -ForegroundColor Green
-    } catch {
-        Throw "`nERROR: Failed to Activate Virtual Environment!`n$($_.Exception.Message)"
-    }
+    $process | Wait-Process
+    $exitCode = $process.ExitCode
 
-    # Run Manga Image Translator in web mode
-    try {
-        Write-Host "`nRunning Manga Image Translator in Web Mode... " -ForegroundColor Yellow
-
-        if ($ServerHost -eq "IP Address") {
-            $Bind = Get-NetIPAddress | Where-Object {$_.AddressFamily -eq 'IPv4' -and $_.IPAddress -like '192.168.1.*'} | Select-Object -ExpandProperty IPAddress
-        } else {
-            $Bind = $ServerHost
-        }
-
-        python .\server\main.py --host $Bind --port $Port
-
-        if ($LASTEXITCODE -ne 0) {
-            Throw "Manga Image Translator Run into Exception!`nEXIT CODE: $LASTEXITCODE."
-        } else {
-            if ($CleanMITresultFolder) {
-                Get-ChildItem -Path ".\result" -Recurse | Where-Object { $_.Name -notlike "log_*.txt" } | Remove-Item -Recurse -Force -Confirm:$false
-            }
-        }
-    } catch {
-        if ($ServerHost -eq "IP Address") {
-            # handle the dynamic IP change when the server is running
-            $NewBind = Get-NetIPAddress | Where-Object {$_.AddressFamily -eq 'IPv4' -and $_.IPAddress -like '192.168.1.*'} | Select-Object -ExpandProperty IPAddress
-            if ($Bind -ne $NewBind) {
-                python .\server\main.py --host $NewBind  --port $Port
-            }
-        } else {
-            Throw "`nERROR: $($_.Exception.Message)"
+    if ($exitCode -eq 1) {
+        Throw "Manga Image Translator Ran into Exception!`nEXIT CODE: $exitCode."            
+    } else {
+        if ($CleanMITresultFolder) {
+            Get-ChildItem -Path ".\result" -Recurse | Where-Object { $_.Name -notlike "log_*.txt" } | Remove-Item -Recurse -Force -Confirm:$false
         }
     }
     
     Write-Host "`nLauncher Ran Successfully." -ForegroundColor Green
 } catch {
+    if (Test-Path $ErrorLogPath) {
+        Write-Host "`n"
+        Get-Content $ErrorLogPath | ForEach-Object {
+            Write-Host "$_" -ForegroundColor Red
+        }
+        Remove-Item $ErrorLogPath -Force -Confirm:$false
+    }
+
     Write-Host "`n$($_.Exception.Message)`n`nLauncher Ran into Error!" -ForegroundColor Red
+} finally {
+    Remove-Item -Path $MITserverPath -Force -Confirm:$false
 }
+
+# Show exit confirmation
+Write-Host "`nPress Enter to exit" -ForegroundColor Cyan -NoNewLine
+Read-Host
