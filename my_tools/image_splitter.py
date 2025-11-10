3ff201972132e11811c38b769a85029829ee059c
Mayonnaisu <116666368+Mayonnaisu@users.noreply.github.com>
2025-11-11 00:19:04 +0700

    Improve image_splitter.py - Detect the commonest mode of original images for conversion - Fix ZeroDivisionError if there's empty folder in single folder processing

--- my_tools/image_splitter.py
+++ my_tools/image_splitter.py
@@ -24,6 +24,9 @@ def split_images_horizontally(input_root_folder, split_parts, string_to_find, st
 
     Args:
         input_root_folder (str): The root folder containing images to be split.
+        split_parts  (str/int): The number of parts to split
+        string_to_find (str): The string to find in folder name
+        string_to_replace (str): The string to replace in folder name
     """
     image_extensions = ('jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp')
 
@@ -34,7 +37,6 @@ def split_images_horizontally(input_root_folder, split_parts, string_to_find, st
     # Check if image even exists at all
     has_images = False
     walker = os.walk(input_root_folder)
-    
     for _, _, files in walker:
         for filename in files:
             if filename.lower().endswith(image_extensions):
@@ -58,28 +60,57 @@ def split_images_horizontally(input_root_folder, split_parts, string_to_find, st
         current_input_dir = Path(dirpath)
         relative_path = current_input_dir.relative_to(input_root_path)
 
-        # Get the number of files in the corresponding original path for the number of parts to split
+        image_files = [
+            f
+            for f in filenames
+            if f.lower().endswith(image_extensions)
+        ]
+        # Skip if there's no image in current directory
+        if not image_files:
+            continue
+
+        # Get the number of files for the number of parts to split & the commonest extension and mode in the corresponding original path
         current_original_dir = original_root_path / relative_path
 
-        image_counts = {}
+        original_image_counts = {}
         original_extensions = []
-        for p, _, files in os.walk(current_original_dir):
+        original_modes = []
+        for p, _, files in natsorted(os.walk(current_original_dir)):
+            original_image_files = [
+                fl
+                for fl in files
+                if fl.lower().endswith(image_extensions)
+            ]
+            # Skip if there's no image in current directory
+            if not original_image_files:
+                continue
 
             count = 0
             for file in files:
-                # Get the most common original extension
-                original_extension = file.split('.')[-1].lower()
-                original_extensions.append(original_extension.lower())
-                extension_counts = Counter(original_extensions)
-                most_extension, counts = extension_counts.most_common(1)[0]
-
                 if file.lower().endswith(image_extensions):
+                    # Get the most common original extension
+                    original_extension = file.split('.')[-1].lower()
+                    original_extensions.append(original_extension)
+                    original_extension_counts = Counter(original_extensions)
+                    common_original_extension, counts = original_extension_counts.most_common(1)[0]
+
+                    # Get the most common original mode
+                    original_image_path = current_original_dir / file
+                    try:
+                        with Image.open(original_image_path) as ori_img:
+                            original_modes.append(ori_img.mode)
+                    except Exception as e:
+                        raise Exception(f"Error processing original image '{input_image_path}': {e}")
+                    
+                    # Get original image count
                     count += 1
-            image_counts[p] = count
+            original_image_counts[p] = count
+            original_mode_counts = Counter(original_modes)
+            common_original_mode, _ = original_mode_counts.most_common(1)[0]
 
             # Set the number of split parts according to the given argument
             if split_parts.lower() == "original":
-                parts = image_counts[p]
+                parts = original_image_counts[p]
             else:
                 parts = int(split_parts)
                 if parts < 1:
@@ -105,7 +136,7 @@ def split_images_horizontally(input_root_folder, split_parts, string_to_find, st
                         # Split the image into horizontal parts
                         for i in range(parts):
                             # Construct the output filename
-                            output_filename = f"image{str(i+1).zfill(2)}.{most_extension}"
+                            output_filename = f"image{str(i+1).zfill(2)}.{common_original_extension}"
                             output_image_path = current_output_dir / output_filename
 
                             # Skip splitting if the output image already exists
@@ -123,7 +154,10 @@ def split_images_horizontally(input_root_folder, split_parts, string_to_find, st
                             if i == parts - 1:
                                 lower = height
 
-                            img = img.convert('RGB') # Convert mode to support .jpg conversion
+                            # Convert to the commonest original mode
+                            img = img.convert(common_original_mode)
+
+                            # Crop and save
                             cropped_img = img.crop((left, upper, right, lower))
                             
                             cropped_img.save(output_image_path)
